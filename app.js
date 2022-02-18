3210b53824cac48774d7631988f0b5032cbef159
Dev_Shiv <im.shiv.579@gmail.com>
2022-02-18 21:22:56 +0530

    fetching and showing latest posts from data

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const Data_URL = "./appData.json";
-const HelpSectionPostsWrapper = document.querySelector(".help-posts-wrapper");
+const helpPostSection = document.querySelector(".help-posts-wrapper");
+const latestPostSection = document.getElementById("blogItemsContainer");
 
 getPosts(Data_URL);
-
 async function getPosts(url) {
   const responce = await fetch(url);
   const data = await responce.json();
-  showPosts(data.helpSection);
+  showHelpPosts(data.helpSection);
+  showLatestPosts(data.latestBlogPosts);
 }
-function showPosts(Posts) {
-  HelpSectionPostsWrapper.innerHTML = "";
+function showHelpPosts(Posts) {
+  helpPostSection.innerHTML = "";
   Posts.forEach((Post) => {
     const { title, imgURL, description } = Post;
     const PostEl = document.createElement("div");
@@ -30,6 +31,31 @@ function showPosts(Posts) {
       </div>
     </div>
           `;
-    HelpSectionPostsWrapper.appendChild(PostEl);
+    helpPostSection.appendChild(PostEl);
+  });
+}
+function showLatestPosts(Posts) {
+  latestPostSection.innerHTML = "";
+  Posts.forEach((Post) => {
+    const { title, imgURL, description } = Post;
+    const PostEl = document.createElement("div");
+    PostEl.classList.add("col");
+    PostEl.innerHTML = `
+      <div class="card h-100 b-6 overflow-hidden">
+      <img
+        src="${imgURL}"
+        class="card-img-top"
+        alt="${title}"
+      />
+      <div class="card-body  py-4 ">
+        <h5 class="card-title cl-primary-800">${title}</h5>
+        <p class="card-text cl-battelshipGrey-600">
+        ${description}
+        </p>
+        <a href="#" class="card-link link">Card link <i class="bi bi-arrow-right"></i></a>
+      </div>
+    </div>
+          `;
+    latestPostSection.appendChild(PostEl);
   });
 }
