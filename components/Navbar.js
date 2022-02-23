bd1063ddc084497a7ee0d01b2c00675409915929
Dev_Shiv <im.shiv.579@gmail.com>
2022-02-23 21:58:31 +0530

    Add: navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,63 @@
+const navbar = document.getElementById("navbar");
+
+Navbar();
+function Navbar() {
+  navbar.innerHTML = "";
+  const div = document.createElement("div");
+  div.classList.add("container");
+  div.innerHTML = `
+    <a class="navbar-brand" href="#">
+            <div class="brand-logo">
+              <img
+                class="img-fluid"
+                src="./images/BrandLogo-dark (2).svg"
+                alt=""
+                srcset=""
+              />
+            </div>
+          </a>
+          <button
+            class="navbar-toggler rounded-pill"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"
+                  >About Us</a
+                >
+              </li>
+              <li class="nav-item ms-md-1">
+                <a class="nav-link" href="#">Carrers</a>
+              </li>
+              <li class="nav-item ms-md-1">
+                <a class="nav-link" href="#">Services</a>
+              </li>
+              <li class="nav-item ms-md-1">
+                <a class="nav-link" href="#">Blog</a>
+              </li>
+              <li class="nav-item mx-md-1">
+                <a class="nav-link" href="#">Contact us</a>
+              </li>
+            </ul>
+            <a
+              target="_"
+              href="https://github.com/imShivam579/Agency-Website"
+              class="btn btn-outline-warning rounded-pill bg-brand-primayfaint"
+            >
+              Clone App
+            </a>
+          </div>
+    `;
+  navbar.appendChild(div);
+}
