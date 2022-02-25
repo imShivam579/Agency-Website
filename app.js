4e07f1add82da94bc0770bdd51f7fe4c45a34e1f
Dev_Shiv <im.shiv.579@gmail.com>
2022-02-25 19:22:08 +0530

    Resolving error 404 - trial 5

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Data_URL = "../appData.json";
+const Data_URL = "./appData.json";
 const videoSection = document.getElementById("videoSection");
 const footer = document.getElementById("footer");
 const postsSection = document.querySelectorAll("[data-posts-category]");
