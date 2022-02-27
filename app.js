96a6c3990577c18c1111e4efe41a9b623a585cf3
Dev_Shiv <im.shiv.579@gmail.com>
2022-02-28 01:25:38 +0530

    Add: footer to Apply page

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const Data_URL = "./appData.json";
 const videoSection = document.getElementById("videoSection");
 const footer = document.getElementById("footer");
 const postsSection = document.querySelectorAll("[data-posts-category]");
+
+// document
+//   .getElementById("applyFormSubmitBtn")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//   });
